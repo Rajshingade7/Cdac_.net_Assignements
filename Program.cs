0607c98e6b9f1798f8d9763e33a5f2bcf71e4684
Raj Shingade <97889220+Rajshingade7@users.noreply.github.com>
2023-12-08 23:55:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    /*internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string o= Console.ReadLine();
+            int a = int.Parse(n);
+            int b=int .Parse(o);
+            int c = a + b;
+            Console.WriteLine(c);
+
+        }
+    }*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a=Console.ReadLine();
+
+            //With the help of parse
+
+            /*bool f=float.TryParse(a, out var b);
+            if(f==true)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }*/
+            //
+
+            //In case of parse it is used
+
+            /*int a1 = int.Parse(a);
+            Console.WriteLine(a1);*/
+
+            //With the help of null here when we made string null it will return 0 instead of throwing exception
+
+            /*a = null;
+            int a2 = Convert.ToInt32(a);
+            Console.WriteLine(a2);*/
+        }
+    }
+    
+}
---
+++ calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class calculator
+    {
+        public int square(int x)
+        {
+            return x * x;
+        }
+        public int cube(int x)
+        {
+            return x * x*x;
+
+        }
+        public int round(float x)
+        {
+            return (int)x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello brother");
+
+
+        }
+    }
+}
